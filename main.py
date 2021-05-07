9ae657e16c74cb9de20b0a4a5fff26d04d94c3ae
Victor Morei <mcore91@yandex.ru>
2021-05-07 18:29:28 +0300

    Translation functionality

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from google_trans_new import google_translator
 
 TOKEN = '1787783787:AAHw8Nw4aieDmt0Dub7oiEjCgCkFIvmzXvA'
+
+target_language = 'ru'
+translator = google_translator() 
 
 def start(update, context):
     update.message.reply_text('Hi...')
@@ -11,12 +15,17 @@ def help(update, context):
 
 
 def translate(update, context):
-    update.message.reply_text(update.message.text)
+    if target_language == '':
+        result = 'You must specify target language'
+    else:
+        result = translator.translate(update.message.text, lang_tgt=target_language)
+    update.message.reply_text(result)
 
+def set_language(update, context):
+    update.message.reply_text('Language set: ')
 
 def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Something went wrong!')
+    update.message.reply_text('Something went wrong!')
 
 
 def main():
@@ -24,6 +33,7 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("set_language", set_language))
     dp.add_handler(MessageHandler(Filters.text, translate))
     dp.add_error_handler(error)
     updater.start_polling()
