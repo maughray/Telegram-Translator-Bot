527d156f947bf8bf1d4b9b4fd4c12680256fa771
Victor Morei <mcore91@yandex.ru>
2021-05-10 22:41:53 +0300

    Taget language selection

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from google_trans_new import google_translator
+import logging
 
-TOKEN = '1787783787:AAHw8Nw4aieDmt0Dub7oiEjCgCkFIvmzXvA'
 
-target_language = 'ru'
+TELEGRAM_TOKEN = '1787783787:AAHw8Nw4aieDmt0Dub7oiEjCgCkFIvmzXvA'
+TARGET_LANGUAGE_KEY = 'target_language'
 translator = google_translator() 
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 def start(update, context):
     update.message.reply_text('Hi...')
@@ -15,21 +21,27 @@ def help(update, context):
 
 
 def translate(update, context):
-    if target_language == '':
-        result = 'You must specify target language'
-    else:
+    if TARGET_LANGUAGE_KEY in context.user_data.keys():
+        target_language = context.user_data[TARGET_LANGUAGE_KEY]
         result = translator.translate(update.message.text, lang_tgt=target_language)
-    update.message.reply_text(result)
+        update.message.reply_text(result)
+    else:
+        update.message.reply_text('You must specify target language!')
+    
 
-def set_language(update, context):
-    update.message.reply_text('Language set: ')
+def set_language(update, context): 
+    language = update.message.text.split()[1]
+    context.user_data[TARGET_LANGUAGE_KEY] = language # TODO: validate language
+    update.message.reply_text('Language set: ' + language)
+
 
 def error(update, context):
     update.message.reply_text('Something went wrong!')
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
